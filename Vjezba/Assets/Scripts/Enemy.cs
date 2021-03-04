4b3b039f09796459f26306ccc0d1cdf46459fd7c
Screw-hub <Screw-hub@users.noreply.github.com>
2021-03-04 16:35:09 +0100

    aaa

---
+++ Vjezba/Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Vector2 moveDirection;
+    public Vector2 lastMoveDirection;
+    private void ProcessInputs ()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if ((moveX == 0 && moveY == 0) && (moveDirection.x != 0 || moveDirection.y != 0))
+        {
+            Debug.Log(moveDirection.x + " " + moveDirection.y);
+            lastMoveDirection = moveDirection;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

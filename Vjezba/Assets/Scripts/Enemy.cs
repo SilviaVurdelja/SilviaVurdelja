7ef110795fc6b7e16a491a38aacaac8e52bda438
Screw-hub <Screw-hub@users.noreply.github.com>
2021-03-18 16:41:57 +0100

    z

--- Vjezba/Assets/Scripts/Enemy.cs
+++ Vjezba/Assets/Scripts/Enemy.cs
@@ -4,19 +4,6 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public Vector2 moveDirection;
-    public Vector2 lastMoveDirection;
-    private void ProcessInputs ()
-    {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-        if ((moveX == 0 && moveY == 0) && (moveDirection.x != 0 || moveDirection.y != 0))
-        {
-            Debug.Log(moveDirection.x + " " + moveDirection.y);
-            lastMoveDirection = moveDirection;
-        }
-    }
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +14,13 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Bullet")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Vjezba/Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    public Animator anim;
+    public GameObject projectile;
+    private Vector2 moveDirection;
+    private bool isRight = true;
+    private bool Vector3;
+    private void ProcessInputs ()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if ((moveX == 0 && moveY == 0) && (moveDirection.x != 0 || moveDirection.y != 0))
+        {
+            Debug.Log(moveDirection.x + " " + moveDirection.y);
+        }
+        moveDirection = new Vector2(moveX, moveY); 
+    }
+
+    private void Animate()
+    {
+        anim.SetFloat("AnimMoveX", moveDirection.x);
+        anim.SetFloat("AnimMoveY", moveDirection.y);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+        Animate();
+
+        if (Input.GetKeyDown("K"))
+        {
+            var obj = Instantiate(projectile, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity) as GameObject;
+            obj.GetComponent<Projectile>().isRight = isRight;
+            if (isRight)
+            {
+                obj.transform.position = new Vector3(transform.position.x + 1, 7f, transform.position.y, 0);
+            }
+            else
+            {
+                obj.transform.position = new Vector3(transform.position.x = 1, 7f, transform.position.y, 0);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
+    }
+}
